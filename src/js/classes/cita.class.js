f097c5581bafe8ed59bccbdfeabcc046e4fc39ad
Paul Chávez <paulchavezromero20@gmail.com>
2020-10-13 20:49:56 -0500

    Classes added

---
+++ src/js/classes/cita.class.js
@@ -0,0 +1,5 @@
+export class Cita {
+    constructor() {
+        this.citas = [];
+    }
+}
---
+++ src/js/classes/index.js
@@ -0,0 +1,7 @@
+import { Cita } from './cita.class.js';
+import { UI } from './ui.class.js';
+
+export {
+    Cita,
+    UI
+}
